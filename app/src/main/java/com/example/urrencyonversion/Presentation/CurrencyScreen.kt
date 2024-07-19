c709dfa0e8603f1adf42159c8f0196d4040044d3
Haggai Khafizov <hafyok5777@gmail.com>
2024-07-19 17:03:31 +0300

    Add text field

--- app/src/main/java/com/example/urrencyonversion/Presentation/CurrencyScreen.kt
+++ app/src/main/java/com/example/urrencyonversion/Presentation/CurrencyScreen.kt
@@ -1,14 +1,17 @@
 package com.example.urrencyonversion.Presentation
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -29,60 +32,46 @@ fun CurrencyScreen(viewModel: CurrencyViewModel = CurrencyViewModel(), modifier:
     LaunchedEffect(Unit) {
         viewModel.fetchRates()
     }
-    
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .padding(32.dp)
-    ){
-        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = !expanded}) {
-            if (error != null) {
-                TextField(value = "Error: $error",
-                    onValueChange = {},
-                    readOnly = true,
-                    modifier = Modifier.menuAnchor()
-                )
-            } else {
-                TextField(value = "RUB",
-                    onValueChange = {},
-                    readOnly = true,
-                    trailingIcon = {ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)},
-                    modifier = Modifier.menuAnchor()
-                )
+    Column {
+        Spacer(modifier = Modifier.padding(vertical = 16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp) // Отступ между элементами
+        ) {
+            OutlinedTextField(
+                value = if (error != null) "Error: $error" else "No Error",
+                onValueChange = {},
+                //readOnly = true,
+                modifier = Modifier.weight(1f) // Задает равную ширину
+            )
 
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded },
+                modifier = Modifier.weight(1f) // Задает равную ширину
+            ) {
+                OutlinedTextField(
+                    value = "RUB",
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    modifier = Modifier.menuAnchor()
+                )
 
                 ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
                     rates.forEach { item ->
                         DropdownMenuItem(
                             text = {
                                 Text("${item.key}: ${item.value.value}")
-                                   },
+                            },
                             onClick = { /*TODO*/ })
                     }
                 }
-                
-                /*LazyColumn {
-                    items(rates.entries.toList()) { entry ->
-                        Text(text = "${entry.key}: ${entry.value.value}")
-                    }
-                }*/
             }
         }
     }
-    
-
-    /*Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Currency Converter", style = MaterialTheme.typography.labelLarge)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        if (error != null) {
-            Text(text = "Error: $error")
-        } else {
-            LazyColumn {
-                items(rates.entries.toList()) { entry ->
-                    Text(text = "${entry.key}: ${entry.value.value}")
-                }
-            }
-        }
-    }*/
 }
